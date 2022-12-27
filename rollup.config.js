413b363f0a13e8c1301b42c46a6e87967894dfc3
Takumi Stryder <nya.takumi.666@gmail.com>
2022-12-27 11:16:45 -0300

    Minified and Unminified settings

--- rollup.config.js
+++ rollup.config.js
@@ -3,47 +3,82 @@ const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 const babel = require('@rollup/plugin-babel');
 const terser = require('@rollup/plugin-terser');
-const packageJson = require('./package.json');
 
-module.exports = {
-    input: './src/index.js',
-    output: [
-        {
-            file: packageJson.main,
-            format: 'cjs',
-            sourcemap: true
-        },
-        {
-            file: packageJson.module,
-            format: 'esm',
-            sourcemap: true
-        },
-        {
-            file: packageJson.browser,
-            format: 'umd',
-            name: 'ReactRUNInput',
-            globals: {
-                react: 'React'
-            },
-            sourcemap: true
-        }
-    ],
-    plugins: [
-        PeerDepsExternalPlugin(),
-        nodeResolve({
-            extensions: [
-                '.js',
-                '.jsx'
-            ]
-        }),
-        commonjs(),
-        babel({
-            exclude: 'node_modules/**',
-            presets: [
-                '@babel/preset-react'
-            ],
-            babelHelpers: 'bundled'
-        }),
-        terser()
+const nodeResolveConfig = {
+    extensions: [
+        '.js',
+        '.jsx'
     ]
 };
+
+const babelConfig = {
+    exclude: 'node_modules/**',
+    presets: [
+        '@babel/preset-react'
+    ],
+    babelHelpers: 'bundled'
+};
+
+module.exports = [
+    {
+        input: './src/index.js',
+        output: [
+            {
+                file: 'dist/cjs/react-run-input.js',
+                format: 'cjs',
+                sourcemap: true
+            },
+            {
+                file: 'dist/esm/react-run-input.js',
+                format: 'esm',
+                sourcemap: true
+            },
+            {
+                file: 'dist/umd/react-run-input.js',
+                format: 'umd',
+                name: 'ReactRUNInput',
+                globals: {
+                    react: 'React'
+                },
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            PeerDepsExternalPlugin(),
+            nodeResolve(nodeResolveConfig),
+            commonjs(),
+            babel(babelConfig)
+        ]
+    },
+    {
+        input: './src/index.js',
+        output: [
+            {
+                file: 'dist/cjs/react-run-input.min.js',
+                format: 'cjs',
+                sourcemap: true
+            },
+            {
+                file: 'dist/esm/react-run-input.min.js',
+                format: 'esm',
+                sourcemap: true
+            },
+            {
+                file: 'dist/umd/react-run-input.min.js',
+                format: 'umd',
+                name: 'ReactRUNInput',
+                globals: {
+                    react: 'React'
+                },
+                sourcemap: true
+            }
+        ],
+        plugins: [
+            PeerDepsExternalPlugin(),
+            nodeResolve(nodeResolveConfig),
+            commonjs(),
+            babel(babelConfig),
+            terser()
+        ]
+    }
+];
