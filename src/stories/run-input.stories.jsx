d58d49a0885481dafabc533965814b2f008cda6e
Takumi Stryder <nya.takumi.666@gmail.com>
2025-03-16 16:13:02 -0300

    feat: sets data type docs and nullish coalescing

--- src/stories/run-input.stories.jsx
+++ src/stories/run-input.stories.jsx
@@ -6,8 +6,14 @@ export default {
   component: RUNInput
 };
 
+/**
+ * @param {{
+ *   value: string;
+ * }} args
+ * @returns {React.JSX.Element}
+ */
 const Template = (args) => {
-  const [value, setValue] = useState(args.value || '');
+  const [value, setValue] = useState(args.value ?? '');
 
   return (
     <RUNInput
