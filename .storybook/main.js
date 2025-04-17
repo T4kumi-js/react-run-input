67d6131dd2faf9932b901be515d0cf6eccccad10
Takumi Stryder <nya.takumi.666@gmail.com>
2025-04-17 16:25:45 -0400

    refactor: code style

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
-  'stories': [
+  stories: [
     '../src/**/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx)'
   ],
-  'addons': [
+  addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-mdx-gfm'
   ],
-  'framework': {
+  framework: {
     name: '@storybook/react-webpack5',
     options: {}
   },
